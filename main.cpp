e0fa0eb40400331860bec9cf2a6f9cf7bc1a7c02
JordanJazz24 <110859178+JordanJazz24@users.noreply.github.com>
2023-10-03 18:45:40 -0600

    fix2

--- main.cpp
+++ main.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
-#include <Windows.h>
-#include <conio.h> // Para _getch()
 #include "Data/List.h"
 
 int main() {
     List* l = new List(1);
     l->printGrid();
     std::cout << std::endl;
+    char mov; // Cambiamos el tipo de variable a char para capturar una letra
 
     while (true) {
-        std::cout << "Presione una tecla (W: Arriba, S: Abajo, A: Izquierda, D: Derecha, Q: Salir): ";
-
-        char mov = _getch(); // Utilizamos _getch() para capturar una tecla sin necesidad de presionar "Enter"
+        std::cout << "Ingrese movimiento (W: Arriba, S: Abajo, A: Izquierda, D: Derecha, Q: Salir): ";
+        std::cin >> mov;
 
         if (mov == 'Q' || mov == 'q') {
             break; // Salir del ciclo si el usuario ingresa 'Q' o 'q'
@@ -19,7 +17,7 @@ int main() {
 
         Movement movement;
 
-        // Convertir la tecla ingresada en un movimiento válido
+        // Convertir la letra ingresada en un movimiento válido
         switch (mov) {
             case 'W':
             case 'w':
@@ -38,14 +36,14 @@ int main() {
                 movement = RIGHT;
                 break;
             default:
-                std::cout << "Tecla no válida. Por favor, ingrese W, S, A, D o Q." << std::endl;
-                continue; // Continuar al siguiente ciclo si la tecla no es válida
+                std::cout << "Movimiento no válido. Por favor, ingrese W, S, A, D o Q." << std::endl;
+                continue; // Continuar al siguiente ciclo si el movimiento no es válido
         }
 
         // Mover el personaje
         l->movePlayer(movement);
 
-        // Limpiar la consola antes de imprimir el nuevo estado del tablero
+        // Imprimir el nuevo estado del tablero
 
         l->printGrid();
         std::cout << std::endl;
