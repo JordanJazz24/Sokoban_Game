0bfa96395598fe217b1f0d82f3cd25222bea0856
JordanJazz24 <110859178+JordanJazz24@users.noreply.github.com>
2023-10-07 21:26:08 -0600

    replay implementado

--- Data/List.h
+++ Data/List.h
@@ -46,6 +46,7 @@ public:
     bool isBoxInPoint(Node* dirNode);
     void swapSymbols(Node*& node);
     char** getLevel(int level);
+
     void resetLevel(int level);
 
 
--- GameLogic/GameMenu.h
+++ GameLogic/GameMenu.h
@@ -2,10 +2,16 @@
 #include <Windows.h>
 #include <conio.h> // Para _getch()
 #include "../Data/List.h"
+struct ReplayMove {
+    Movement movement;
+    // Agrega cualquier otra información necesaria aquí
+};
 
 class GameMenu {
 public:
-    GameMenu() : game(nullptr), currentLevel(1){}
+    GameMenu() : game(nullptr), currentLevel(1){
+        movements = new vector<Movement>();
+    }
 
     void run() {
         while (true) {
@@ -44,6 +50,7 @@ public:
 private:
     List* game;
     int currentLevel = 1;
+    vector<Movement> *movements;
 
     void displayMenu() {
         system("cls"); // Limpiar la consola
@@ -54,7 +61,15 @@ private:
         std::cout << "Elija una opción: ";
     }
 
-
+    void replay(){
+        startNewGame(currentLevel);
+      for (int i = 0; i < movements->size(); ++i) {
+          game->movePlayer(movements->at(i));
+          Sleep(100);
+          system("cls");
+          game->printGrid();
+      }
+    }
 
     void startNewGame( int level) {
         if (game != nullptr) {
@@ -85,22 +100,33 @@ private:
                 case 'W':
                 case 'w':
                     movement = UP;
+                    movements->push_back(movement);
+
                     break;
                 case 'S':
                 case 's':
                     movement = DOWN;
+                    movements->push_back(movement);
+
                     break;
                 case 'A':
                 case 'a':
                     movement = LEFT;
+                    movements->push_back(movement);
+
                     break;
                 case 'D':
                 case 'd':
                     movement = RIGHT;
+                    movements->push_back(movement);
+
                     break;
                 case 'R':
                 case 'r':
-                    game->resetLevel(currentLevel);
+                    game->  resetLevel(currentLevel);
+                    if (!movements->empty()) {
+                        movements->clear();
+                    }
                     break;
                 default:
                     std::cout << "Tecla no válida. Por favor, ingrese W, S, A, D, R o Q." << std::endl;
@@ -109,8 +135,14 @@ private:
                     continue; // Continuar al siguiente ciclo si la tecla no es válida
             }
 
-            // Mover el personaje
-            game->movePlayer(movement);
+            if(mov != 'R' && mov != 'r'){
+                game->movePlayer(movement);
+            }
+            else{
+                system ("cls");
+                continue;
+            }
+
 
             system ("cls");
             if (game->goalStack->size() == game->numBoxes) {
@@ -119,27 +151,69 @@ private:
                 if (currentLevel >= 3) {
                     std::cout << "¡Completaste todos los niveles, sos el puto amo lml !" << std::endl;
                     system("pause");
-                    currentLevel = 1;
-                    return; // Salir del juego
+                    std::cout << "Presione Enter para continuar al reiniciar el juego o 'R' para ver la repeticion del ultimo nivel: \n";
+                    system("pause");
+                }else {
+                    std::cout << "Presione Enter para continuar al siguiente nivel o 'Q' para salir: \n";
+                    std::cout << "--Si deseA ver una repeticion presione 'R'--\n";
                 }
-                currentLevel++; // Avanzar al siguiente nivel
-                std::cout << "Presione Enter para continuar al siguiente nivel o 'Q' para salir: \n";
+
+
+
                 char continueChoice = _getch();
-                while (continueChoice != 'Q' && continueChoice != 'q' && continueChoice != 13) {
-                    std::cout << "Opción no válida. Por favor, presione Enter o 'Q'." << std::endl;
+                while(continueChoice != 'R' && continueChoice != 'r' && continueChoice != 'Q' && continueChoice != 'q' && continueChoice != 13){
+                    if (currentLevel >= 3) {
+                        std::cout << "Opción no válida. Por favor, ingrese R o presione ENTER para reinciiar el juego\n";}
+                    else {
+                        std::cout << "Opción no válida. Por favor, ingrese R o Q, o  presion ENTER" << std::endl;
+                    }
                     continueChoice = _getch();
                 }
-                if (continueChoice == 'Q' || continueChoice == 'q') {
-                    return; // Salir del juego
-                } else {
 
-                    system ("cls");
-                    startNewGame(currentLevel); // Cargar el siguiente nivel
+                while (continueChoice == 'R' || continueChoice == 'r'){
+                    replay();
+                    if (currentLevel >= 3) {
+                        std::cout << "¡Completaste todos los niveles, sos el puto amo lml !" << std::endl;
+                        std::cout << "Presione Enter para reiniciar el juego 'R' volver a ver la Repeticion: \n";
+                    }else {
+                        std::cout << "Presione Enter para continuar al siguiente nivel o 'Q' para salir: \n";
+                        std::cout << "--Si deseA ver una repeticion presione 'R'--\n";
+                    }
+
+                    continueChoice = _getch();
+                    while(continueChoice != 'R' && continueChoice != 'r' && continueChoice != 'Q' && continueChoice != 'q' && continueChoice != 13){
+                        if (currentLevel >= 3) {
+                            std::cout << "Opción no válida. Por favor, ingrese R o presione ENTER para reinciiar el juego\n";}
+                        else {
+                            std::cout << "Opción no válida. Por favor, ingrese R o Q, o  presion ENTER" << std::endl;
+                        }
+                        continueChoice = _getch();
+                    }
                 }
+
+                if (continueChoice == 'Q' || continueChoice == 'q') {
+                    currentLevel = 1;
+                    startNewGame(currentLevel);// reiniciar el juego
+                    movements->clear();
+                    return; // Salir del juego
+                }
+                else{
+                    if (currentLevel >= 3) {
+                        currentLevel = 1;
+                        startNewGame(currentLevel);// reiniciar el juego
+                        movements->clear();
+                        return; // Salir del juego
+                    }
+                    currentLevel++; // Avanzar al siguiente nivel
+                    movements->clear();
+                    system ("cls");
+                    startNewGame(currentLevel); }// Crear una nueva instancia del juego
             }
             std::cout << std::endl;
         }
     }
+
+
 
 };
 
